67628bbdce3b84286016e0b3ef7168945caeeaae
BossioCervantes <124269531+BossioCervantes@users.noreply.github.com>
2023-03-09 11:05:05 -0500

    Add files via upload

---
+++ ParcialCorte1/javaS.js
@@ -0,0 +1,112 @@
+function Geometria (vx) {
+    geom = new TRHEE.Geometry();
+    var LargoVertice = vx.lenght;
+    for (i=0; i<LargoVertice; i++) {
+        [x,y,z] = [vx[i][0], vx[i][1],vx[i][2]];
+        vector = new TRHEE.Vector3(x,y,z)
+        geom.vertices.push(vector);
+    }
+    return geom;
+}
+function Traslacion (vt) {
+    var MatrizT= new TRHEE.Matrix4();
+    MatrizT.set(
+        1,0,0,vt[0],
+        0,1,0,vt[1],
+        0,0,1,vt[2],
+        0,0,0,1
+    );
+
+    return MatrizT;
+}
+function Escalado (vs) {
+    var MatrizS = new TRHEE.Matrix4();
+    MatrizS.set (
+        vs[0],0,0,0,
+        0,vs[1],0,0,
+        0,0,vs[2],0,
+        0,0,0,1
+    );
+    return MatrizS; 
+}
+function EscaladoReal(fig,posini,vs) {
+     //Vector para llevar al origen
+    tr = [-posini[0],-posini[1],-posini[2]];
+    fig.applyMatrix(Traslacion(tr));
+    fig.applyMatrix(Escalado(vs));
+    fig.applyMatrix(Traslacion(posini)); 
+}
+
+function init() {
+
+    // Escena
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);    
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0x000000, 1.0);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    var size = 700;
+    var arrowSize = 40;
+    var divisions = 20;
+    var origin = new THREE.Vector3( 0, 0, 0 );
+    var x = new THREE.Vector3( 1, 0, 0 );
+    var y = new THREE.Vector3( 0, 1, 0 );
+    var z = new THREE.Vector3( 0, 0, 1 );
+    var color2 = new THREE.Color( 0x333333 ); 
+    var colorR = new THREE.Color( 0xAA0000 );
+    var colorG = new THREE.Color( 0x00AA00 );
+    var colorB = new THREE.Color( 0x0000AA );
+
+    //Crear la Grilla
+    var gridHelperXZ = new THREE.GridHelper( size, divisions, color2, color2);
+
+    //Flechas
+    var arrowX = new THREE.ArrowHelper( x, origin, arrowSize, colorR );
+    var arrowY = new THREE.ArrowHelper( y, origin, arrowSize, colorG );
+    var arrowZ = new THREE.ArrowHelper( z, origin, arrowSize, colorB );
+        
+    //Cámara
+    camera.position.x = 000;
+    camera.position.y = 100;
+    camera.position.z = 400;
+    camera.lookAt(scene.position);
+
+    //Creación de las Figuras
+    //Geometria de la piramide
+    lado = 30;
+    h = 45;
+    [v1,v2,v3,v4,v5] = [[0,0,0][lado,0,0][lado,0,lado][0,0,lado][lado/2,h,lado/2]];
+    var vertices = [v1,v2,v3,v4,v5,v1,v2,v3,v4,v5,v2];
+    geomPiramide = Geometria(vertices);
+//    var largoVertice = vertices.length;
+
+    // Colores para las piramides
+    color = [{color:0xFF0000}, {color:0x00ff00}];
+
+    //material para las piramides
+    material = [];
+    for(i=0;i<2;i++){
+        material.push(new THREE.ParticleBasicMaterial(color[i]));
+    }
+
+    //Figuras para las piramides
+    piramide = [];
+    for(i=0;i<2;i++){
+        piramide.push(new THREE.Line(geomPiramide, material[i]));
+    }
+
+    // En el documento HTML
+    document.body.appendChild(renderer.domElement);
+
+    scene.add(gridHelperXZ);
+    scene.add(ArrowX);
+    scene.add(ArrowY);
+    scene.add(ArrowZ);
+    for  (i=0; i < 2; i++);
+        scene.add(piramide[i]);
+
+    renderer.render(scene, camera);
+}
+
+init();
